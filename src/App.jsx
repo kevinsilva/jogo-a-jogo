6aeb75c050357290e44e99628dd92c2da2126799
kevinsilva <kevinsilva.j@gmail.com>
2023-01-05 23:17:21 +0000

    adds variables and mixin files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './styles/App.scss';
+
 import MatchesRow from './components/MatchesRow';
 
 function App() {
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,20 +1,21 @@
 import formatDate from '../utils/utilities';
+import '../styles/ScoreCard.scss';
 
 export default function ScoreCard({ matchData }) {
   return (
-    <div className="card-container">
-      <div className="">
-        <h4>{matchData.league.name}</h4>
-        <h4>{formatDate(matchData.fixture.date)}</h4>
+    <div className="card">
+      <div className="card__header">
+        <h4 className="card__league-name">{matchData.league.name}</h4>
+        <h4 className="card__date">{formatDate(matchData.fixture.date)}</h4>
       </div>
       <div>
-        <div>
-          <h3>{matchData.teams.home.name}</h3>
-          <h3>{matchData.goals.home}</h3>
+        <div className="card__home-section">
+          <h3 className="card__home-name">{matchData.teams.home.name}</h3>
+          <h3 className="card__home-goals">{matchData.goals.home}</h3>
         </div>
-        <div>
-          <h3>{matchData.teams.away.name}</h3>
-          <h3>{matchData.goals.away}</h3>
+        <div className="card__away-section">
+          <h3 className="card__away-name">{matchData.teams.away.name}</h3>
+          <h3 className="card__away-goals">{matchData.goals.away}</h3>
         </div>
       </div>
     </div>
