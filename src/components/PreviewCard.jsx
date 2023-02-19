f20e789be8bc59c104a57ef2dc3d0469d1c0b125
kevinsilva <kevinsilva.j@gmail.com>
2023-02-19 16:16:21 +0000

    refactors matchcards css

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -4,35 +4,35 @@ export default function PreviewCard({ previewData }) {
   return (
     <div className="match-card">
       <div className="match-card__header">
-        <h4 className="match-card__league-name">{previewData.league.name}</h4>
+        <h4 className="match-card__league">{previewData.league.name}</h4>
         <h4 className="match-card__date">
           {formatDate(previewData.fixture.date)}
         </h4>
       </div>
-      <div className="match-card__sections match-card__sections--preview">
+      <div className="match-card__main match-card__main--preview">
         <div className="match-card__teams">
           <div className="match-card__home">
-            {/* <img
+            <img
               className="match-card__home-img"
               src={previewData.teams.home.logo}
               alt=""
-            /> */}
+            />
             <h3 className="match-card__home-name">
               {previewData.teams.home.name}
             </h3>
           </div>
           <div className="match-card__away">
-            {/* <img
+            <img
               className="match-card__away-img"
               src={previewData.teams.away.logo}
               alt=""
-            /> */}
+            />
             <h3 className="match-card__away-name">
               {previewData.teams.away.name}
             </h3>
           </div>
         </div>
-        <div className="match-card__tv-info">
+        <div className="match-card__tv">
           <h4 className="match-card__tv-name">
             {LEAGUES[previewData.league.name]?.tvChannel[0] ?? 'Not defined'}
           </h4>
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -4,24 +4,24 @@ export default function ScoreCard({ scoreData }) {
   return (
     <div className="match-card">
       <div className="match-card__header">
-        <h4 className="match-card__league-name">{scoreData.league.name}</h4>
+        <h4 className="match-card__league">{scoreData.league.name}</h4>
         <h4 className="match-card__date">
           {formatDate(scoreData.fixture.date)}
         </h4>
       </div>
-      <div className="match-card__sections match-card__sections--score">
-        <div className="match-card__home-section">
+      <div className="match-card__main match-card__main--score">
+        <div className="match-card__home">
           <img
-            className="match-card__home-logo"
+            className="match-card__home-img"
             src={scoreData.teams.home.logo}
             alt=""
           />
           <h3 className="match-card__home-name">{scoreData.teams.home.name}</h3>
           <h3 className="match-card__home-goals">{scoreData.goals.home}</h3>
         </div>
-        <div className="match-card__away-section">
+        <div className="match-card__away">
           <img
-            className="match-card__away-logo"
+            className="match-card__away-img"
             src={scoreData.teams.away.logo}
             alt=""
           />
