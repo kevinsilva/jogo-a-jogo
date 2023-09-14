ad490d6c6321a5ea0d7263dbacb9c790a701e96c
kevinsilva <kevinsilva.j@gmail.com>
2023-09-14 13:22:37 +0100

    removes mock comments on Leagues

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import App from './App';
 
 import './styles/main.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App />
-  //</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
